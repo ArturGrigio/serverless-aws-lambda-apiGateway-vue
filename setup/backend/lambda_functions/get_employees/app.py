e6e4040f1782feb0e87e133b8eae1d1f79e2e593
Artur Grigio <fsastudent@yahoo.com>
2018-01-30 20:17:43 -0800

    added a get all employees function

---
+++ setup/backend/lambda_functions/get_employees/app.py
@@ -0,0 +1,40 @@
+import sys
+import logging
+import rds_config
+import pymysql
+import json
+
+#rds settings
+rds_host  = rds_config.db_endpoint
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=3)
+    logger.info("\nSUCCSESS: Connection Established")
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS mysql instance succeeded")
+
+def handler(event, context):
+    output = []
+
+    with conn.cursor() as cur:
+        cur.execute("select * from employees")
+        for row in cur:
+            employee_dict = {
+                'id' : row[0],
+                'first' : row[1],
+                'last' : row[2],
+                'email': row[3]
+            }
+            output.append(employee_dict)
+
+    return output
